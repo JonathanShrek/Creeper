6582341c6aa5b3435df34b4894fe085970bc9c68
Jonathan Shreckengost <jonathanshrek@gmail.com>
2022-09-03 23:04:02 -0400

    Clean up
    Cleaned up the main.rs file by moving non main functions to the lib.rs
    file. Also removed some unneccesary dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 extern crate notify;
-extern crate fs_extra;
 
 use std::process::ExitCode;
 use std::sync::mpsc::channel;
 use std::env;
 use std::path::Path;
-use std::path::PathBuf;
-use std::fs::metadata;
 use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher};
-use fs_extra::dir::copy as dir_copy;
-use fs_extra::file::copy as file_copy;
+use copy_torrents::copy_file;
 
 fn main() -> ExitCode {
     // Get the target directory path from the arguments.
@@ -50,50 +46,12 @@ fn main() -> ExitCode {
         match rx.recv() {
             Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
                 // Only copy file if we have a create op code.
-                if op == notify::op::CREATE
-                {
+                if op == notify::op::CREATE {
                     copy_file(&target_dir, path);
                 }
             },
             Ok(event) => println!("broken event: {:?}", event),
             Err(e) => println!("watch error: {:?}", e),
-        }
-    }
-}
-
-fn copy_file(target_dir: &PathBuf, item_path: PathBuf) {
-    // Used to determine if path is to a file or directory.
-    let md = metadata(&item_path).unwrap();
-
-    // Directory copy options.
-    let mut dir_options = fs_extra::dir::CopyOptions::new();
-    dir_options.copy_inside = true;
-
-    // File copy options.
-    let file_options = fs_extra::file::CopyOptions::new();
-
-    // If directory then copy.
-    if md.is_dir()
-    {
-        match dir_copy(&item_path, target_dir, &dir_options) {
-            Ok(_event) => println!("copied {:?} to {:?}", &item_path, target_dir),
-            Err(e) => println!("copy error: {:?}", e)
-        }
-    }
-
-    // If file then copy.
-    if md.is_file()
-    {
-        let file_name = item_path.file_name().unwrap();
-
-        // Create the new file path.
-        let mut new_file_path = PathBuf::new();
-        new_file_path.push(target_dir);
-        new_file_path.push(file_name);
-
-        match file_copy(&item_path, new_file_path, &file_options) {
-            Ok(_event) => println!("copied {:?} to {:?}", &item_path, target_dir),
-            Err(e) => println!("copy error: {:?}", e)
         }
     }
 }
