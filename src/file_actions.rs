6f414f1e042245ec66f43b4d00746ffe5e69324e
Jonathanshrek <jonathanshrek@gmail.com>
2024-02-07 14:00:15 -0500

    will now wait for the copy to complete before returning to main thread

--- src/file_actions.rs
+++ src/file_actions.rs
@@ -1,5 +1,7 @@
 extern crate fs_extra;
 
+use crossbeam::channel::unbounded;
+
 use std::{
     thread,
     fs::metadata,
@@ -18,27 +20,32 @@ pub fn copy(target_dir: &PathBuf, item_path: PathBuf) {
     let target_dir_clone = target_dir.clone();
     let item_path_clone = item_path.clone();
 
+    // Set up a channel for communication between threads
+    let (sender, receiver) = unbounded();
+
     // If directory then copy.
     if md.is_dir() {
-        // Spawn a new thread to copy the directory
-        let handler = thread::spawn(move || {
-            let mut dir_options = fs_extra::dir::CopyOptions::new();
-            dir_options.copy_inside = true;
+        let mut dir_options = fs_extra::dir::CopyOptions::new();
+        dir_options.copy_inside = true;
 
+        thread::spawn(move || {
             match dir_copy(&item_path_clone, &target_dir_clone, &dir_options) {
-                Ok(_event) => println!("copied directory {:?} to {:?}", &item_path_clone, target_dir_clone),
+                Ok(_event) => {
+                    println!("copying directory {:?} to {:?}", &item_path_clone, target_dir_clone);
+                    sender.send(()).unwrap();
+                },
                 Err(e) => println!("copy directory error: {:?}", e)
             }
         });
 
         // Wait for the thread to finish and check if it was successful or errored
-        match handler.join() {
+        match receiver.recv() {
             Ok(_) => println!("Thread successfully completed"),
             Err(e) => println!("Thread errored: {:?}", e),
-        }
+        };
     } else if md.is_file() {
         // Spawn a new thread to copy the file
-        let handler = thread::spawn(move || {
+        thread::spawn(move || {
             let file_name = item_path_clone.file_name().unwrap_or_default();
             let mut new_file_path = PathBuf::new();
             new_file_path.push(&target_dir_clone);
@@ -47,13 +54,16 @@ pub fn copy(target_dir: &PathBuf, item_path: PathBuf) {
             let file_options = fs_extra::file::CopyOptions::new();
 
             match file_copy(&item_path_clone, &new_file_path, &file_options) {
-                Ok(_event) => println!("copied file {:?} to {:?}", &item_path_clone, target_dir_clone),
+                Ok(_event) => {
+                    println!("copying file {:?} to {:?}", &item_path_clone, target_dir_clone);
+                    sender.send(()).unwrap();
+                },
                 Err(e) => println!("copy file error: {:?}", e)
             }
         });
 
         // Wait for the thread to finish and check if it was successful or errored
-        match handler.join() {
+        match receiver.recv() {
             Ok(_) => println!("Thread successfully completed"),
             Err(e) => println!("Thread errored: {:?}", e),
         }
